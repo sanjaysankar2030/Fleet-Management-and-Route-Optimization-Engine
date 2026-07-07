6e740f27a92beec0023e06b6d13ad313e8f21a1b
Sanjay Sankar <sanjaysankar2030@gmail.com>
2026-07-07 20:56:51 +0530

    Fixed conflict with DistMatrix

--- src/main/java/com/example/fleet/service/DistMatrixService.java
+++ src/main/java/com/example/fleet/service/DistMatrixService.java
@@ -1,14 +1,14 @@
 package com.example.fleet.service;
 
+import java.util.List;
+
 import com.example.fleet.entity.WayPoint;
+
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.logging.Logger;
 
 @Service
 public class DistMatrixService {
